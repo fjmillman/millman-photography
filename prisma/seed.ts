1d046dd7bc1307106317de12d9fe8afcfca41ec3
Freddie John Millman <freddie.millman@icloud.com>
2022-10-30 18:22:44 +0000

    Add S3 handling to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,37 +2,44 @@ import type { Tag, User, Image } from '@prisma/client';
 import { Status } from '@prisma/client';
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt';
-import s3 from '../app/utils/s3';
 import cuid from 'cuid';
-import path from 'path';
 import { readFileSync } from 'fs';
+import path from 'path';
+
+import s3 from '../app/utils/s3';
 
 const prisma = new PrismaClient()
 
 async function main() {
-    const userFixtures = [{
-        email: 'admin@millmanphotography.co.uk',
-        password: 'abc123',
-        firstName: 'Test',
-        lastName: 'Admin',
-        isAdmin: true,
-    }]
+  await prisma.user.deleteMany()
+  await prisma.tag.deleteMany()
+  await prisma.image.deleteMany()
+  await prisma.post.deleteMany()
+  await prisma.gallery.deleteMany()
 
-    const salt = await bcrypt.genSalt(10);
+  const userFixtures = [{
+    email: 'admin@millmanphotography.co.uk',
+    password: 'abc123',
+    firstName: 'Test',
+    lastName: 'Admin',
+    isAdmin: true,
+  }]
+
+  const salt = await bcrypt.genSalt(10);
 
   const users = await Promise.all(userFixtures.map(async ({ password, ...user}) => {
     return await prisma.user.upsert({
-        where: { email: user.email },
-        update: {},
-        create: {
-            ...user,
-            password: await bcrypt.hash(password, salt),
-        },
-      })
+      where: { email: user.email },
+      update: {},
+      create: {
+        ...user,
+        password: await bcrypt.hash(password, salt),
+      },
+    })
   }));
 
-    const getUserFromUsersByEmail = (users: User[], email: string): User => {
-    const user = users.find((user) => user.email === email)
+  const getUserFromUsersByEmail = (users: User[], email: string): User => {
+  const user = users.find((user) => user.email === email)
     if (!user){
       throw new Error(`User with email (${email}) not found`)
     }
@@ -42,10 +49,10 @@ async function main() {
   const tagFixtures = [{ slug: 'landscape', name: 'Landscape' }, { slug: 'architecture', name: 'Architecture' }, { slug: 'portrait', name: 'Portrait' }, { slug: 'travel', name: 'Travel' }]
 
   const tags = await Promise.all(tagFixtures.map((tag) => prisma.tag.upsert({
-        where: { slug: tag.slug },
-        update: {},
-        create: tag,
-      })))
+    where: { slug: tag.slug },
+    update: {},
+    create: tag,
+  })))
 
   const getTagFromTagsBySlug = (tags: Tag[], slug: string): Tag => {
     const tag = tags.find((tag) => tag.slug === slug)
@@ -53,6 +60,32 @@ async function main() {
         throw new Error(`Tag with slug (${slug}) not found`)
     }
     return tag
+  }
+
+  const clearImagesFromS3 = async () => {
+    const { Contents } = await s3
+      .listObjects({
+        Bucket: process.env.S3_BUCKET_NAME || '',
+      })
+      .promise();
+
+    if (!Contents) {
+      return
+    }
+
+    for (const Content of Contents) {
+      const { Key } = Content
+      if (!Key) {
+        continue
+      }
+
+      await s3
+        .deleteObject({
+          Bucket: process.env.S3_BUCKET_NAME || '',
+          Key,
+        })
+        .promise();
+    }
   }
 
   const uploadImageToS3 =  async (filename: string) => {
@@ -68,12 +101,13 @@ async function main() {
       })
       .promise();
 
-      return Location;
+    return Location;
   }
 
-  const image1Url = ''; // await uploadImageToS3('example-1.jpg');
-  const image2Url = ''; // await uploadImageToS3('example-2.jpg');
-  const image3Url = ''; // await uploadImageToS3('example-3.jpg');
+  await clearImagesFromS3()
+  const image1Url = await uploadImageToS3('example-1.jpg')
+  const image2Url = await uploadImageToS3('example-2.jpg')
+  const image3Url = await uploadImageToS3('example-3.jpg')
 
   const imageFixtures = [
     {
@@ -105,88 +139,88 @@ async function main() {
   const getImageFromImagesBySlug = (images: Image[], slug: string): Image => {
     const image = images.find((image) => image.slug === slug)
     if (!image){
-        throw new Error(`Image with slug (${slug}) not found`)
+      throw new Error(`Image with slug (${slug}) not found`)
     }
     return image
   }
 
   const galleryFixtures = [
     {
-        title: 'Landscape Gallery',
-        slug: 'landscape-gallery',
-        description: 'This is a landscape gallery',
-        status: Status.Draft,
-        publishedAt: null,
-        tagId: getTagFromTagsBySlug(tags, 'landscape').id,
-        imageId: getImageFromImagesBySlug(images, 'landscape').id,
-      },
-      {
-        title: 'Architecture Gallery',
-        slug: 'architecture-gallery',
-        description: 'This is an architecture gallery',
-        status: Status.Published,
-        publishedAt: new Date(),
-        tagId: getTagFromTagsBySlug(tags, 'architecture').id,
-        imageId: getImageFromImagesBySlug(images, 'architecture').id,
-      },
-      {
-        title: 'Portrait Gallery',
-        slug: 'portrait-gallery',
-        description: 'This is a portrait gallery',
-        status: Status.Archived,
-        publishedAt: new Date(),
-        tagId: getTagFromTagsBySlug(tags, 'portrait').id,
-        imageId: getImageFromImagesBySlug(images, 'portrait').id,
-      }
+      title: 'Landscape Gallery',
+      slug: 'landscape-gallery',
+      description: 'This is a landscape gallery',
+      status: Status.Draft,
+      publishedAt: null,
+      tagId: getTagFromTagsBySlug(tags, 'landscape').id,
+      imageId: getImageFromImagesBySlug(images, 'landscape').id,
+    },
+    {
+      title: 'Architecture Gallery',
+      slug: 'architecture-gallery',
+      description: 'This is an architecture gallery',
+      status: Status.Published,
+      publishedAt: new Date(),
+      tagId: getTagFromTagsBySlug(tags, 'architecture').id,
+      imageId: getImageFromImagesBySlug(images, 'architecture').id,
+    },
+    {
+      title: 'Portrait Gallery',
+      slug: 'portrait-gallery',
+      description: 'This is a portrait gallery',
+      status: Status.Archived,
+      publishedAt: new Date(),
+      tagId: getTagFromTagsBySlug(tags, 'portrait').id,
+      imageId: getImageFromImagesBySlug(images, 'portrait').id,
+    }
   ]
 
   await Promise.all(galleryFixtures.map(({ tagId, imageId, ...gallery }) => prisma.gallery.upsert({
-        where: { slug: gallery.slug },
-        update: {},
-        create: { ...gallery, tags: { create: { tagId }}, images: { create: { imageId } }},
-      })))
+    where: { slug: gallery.slug },
+    update: {},
+    create: { ...gallery, tags: { create: { tagId }}, images: { create: { imageId } }},
+  })))
 
-      const postFixtures = [
-        {
-            title: 'Welcome to my blog',
-            slug: 'welcome-to-my-blog',
-            description: 'An introduction to who I am',
-            content: '',
-            status: Status.Draft,
-            publishedAt: null,
-            tagId: getTagFromTagsBySlug(tags, 'portrait').id,
-            authorId: getUserFromUsersByEmail(users, 'admin@millmanphotography.co.uk').id,
-            imageId: getImageFromImagesBySlug(images, 'portrait').id,
-          },
-          {
-            title: 'A Trip to Iceland',
-            slug: 'a-trip-to-iceland',
-            description: 'Reflecting on my trip to Iceland',
-            content: '',
-            status: Status.Published,
-            publishedAt: new Date(),
-            tagId: getTagFromTagsBySlug(tags, 'travel').id,
-            authorId: getUserFromUsersByEmail(users, 'admin@millmanphotography.co.uk').id,
-            imageId: getImageFromImagesBySlug(images, 'architecture').id,
-          },
-          {
-            title: 'Landscape Memory Lane',
-            slug: 'landscape-memory-lane',
-            description: 'A walk down memory lane with my landscapes',
-            content: '',
-            status: Status.Archived,
-            publishedAt: new Date(),
-            tagId: getTagFromTagsBySlug(tags, 'landscape').id,
-            authorId: getUserFromUsersByEmail(users, 'admin@millmanphotography.co.uk').id,
-            imageId: getImageFromImagesBySlug(images, 'landscape').id,
-          }
-      ]
-    
-      await Promise.all(postFixtures.map(({ tagId, imageId, ...post }) => prisma.post.upsert({
-            where: { slug: post.slug },
-            update: {},
-            create: { ...post, tags: { create: { tagId }}, images: { create: { imageId } }},
-          })))
+  const postFixtures = [
+    {
+      title: 'Welcome to my blog',
+      slug: 'welcome-to-my-blog',
+      description: 'An introduction to who I am',
+      content: '',
+      status: Status.Draft,
+      publishedAt: null,
+      tagId: getTagFromTagsBySlug(tags, 'portrait').id,
+      authorId: getUserFromUsersByEmail(users, 'admin@millmanphotography.co.uk').id,
+      imageId: getImageFromImagesBySlug(images, 'portrait').id,
+    },
+    {
+      title: 'A Trip to Iceland',
+      slug: 'a-trip-to-iceland',
+      description: 'Reflecting on my trip to Iceland',
+      content: '',
+      status: Status.Published,
+      publishedAt: new Date(),
+      tagId: getTagFromTagsBySlug(tags, 'travel').id,
+      authorId: getUserFromUsersByEmail(users, 'admin@millmanphotography.co.uk').id,
+      imageId: getImageFromImagesBySlug(images, 'architecture').id,
+    },
+    {
+      title: 'Landscape Memory Lane',
+      slug: 'landscape-memory-lane',
+      description: 'A walk down memory lane with my landscapes',
+      content: '',
+      status: Status.Archived,
+      publishedAt: new Date(),
+      tagId: getTagFromTagsBySlug(tags, 'landscape').id,
+      authorId: getUserFromUsersByEmail(users, 'admin@millmanphotography.co.uk').id,
+      imageId: getImageFromImagesBySlug(images, 'landscape').id,
+    }
+  ]
+  
+  await Promise.all(postFixtures.map(({ tagId, imageId, ...post }) => prisma.post.upsert({
+    where: { slug: post.slug },
+    update: {},
+    create: { ...post, tags: { create: { tagId }}, images: { create: { imageId } }},
+  })))
 }
 
 main()
